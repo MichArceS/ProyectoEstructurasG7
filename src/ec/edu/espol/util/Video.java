677ceb5de72d1fddc889c24706af7d9d85b4a428
micxarce <micharcesierra2028@gmail.com>
2020-07-01 20:08:25 -0500

    Construida la clase Video y TurnosView, a falta de futuras modificaciones en la última

--- src/ec/edu/espol/util/Video.java
+++ src/ec/edu/espol/util/Video.java
@@ -1,4 +1,30 @@
 package ec.edu.espol.util;
 
+import javafx.scene.media.Media;
+
 public class Video {
+
+    private String nombre;
+    private Media media;
+
+    public Video(String n, String uri) {
+        nombre = n;
+        media = new Media(uri);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Media getMedia() {
+        return media;
+    }
+
+    public void setMedia(Media media) {
+        this.media = media;
+    }
 }
--- src/ec/edu/espol/views/TurnosView.java
+++ src/ec/edu/espol/views/TurnosView.java
@@ -1,4 +1,75 @@
 package ec.edu.espol.views;
 
-public class TurnosView {
+import ec.edu.espol.util.Video;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.util.Observable;
+import java.util.Observer;
+
+
+public class TurnosView implements Observer {
+
+    private BorderPane root;
+    private VBox sup;
+    private Label bot;
+    private VBox mid;
+    private MediaView mediaView;
+    private VBox boxPuestos;
+
+    public TurnosView(Scene scene) {
+        inicializarObjetos();
+        crearEstructuraSuperior();
+        crearEstructuraInferior();
+        crearEstructuraMid();
+        }
+
+    private void inicializarObjetos() {
+        root = new BorderPane();
+        sup = new VBox();
+        bot = new Label();
+        mid = new VBox();
+        mediaView = new MediaView();
+        boxPuestos = new VBox();
+    }
+
+    private void crearEstructuraMid() {
+        mid.getChildren().addAll(mediaView,boxPuestos);
+    }
+
+    private void crearEstructuraSuperior() {
+        Image image = new Image("Ejemplo");
+        ImageView imageView = new ImageView(image);
+        imageView.setScaleX(50);
+        imageView.setScaleY(50);
+        Label label = new Label("Hora de ejemplo");
+        sup.getChildren().addAll(imageView,label);
+    }
+
+    private void crearEstructuraInferior() {
+        bot.setText("Bienvenidos! Los horarios de atención son: lunes a viernes de 6:00 -- 18:00");
+    }
+
+    private void setVideo(Video video) {
+        Media media = video.getMedia();
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+    }
+
+    private void añadirTurnos() {
+
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        Video video = (Video)o;
+        setVideo(video);
+    }
 }
